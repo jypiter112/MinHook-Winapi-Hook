c6f8a8f0ebb39774a8dd091bb40c810e2ac4e8de
jypiter112 <146531209+jypiter112@users.noreply.github.com>
2024-11-06 18:41:39 +0200

    Update hookmain.cpp

--- Project1/hookmain.cpp
+++ Project1/hookmain.cpp
@@ -1,6 +1,6 @@
-#include <Windows.h>
 #include <iostream>
 #include <MinHook.h>
+
 #pragma comment(lib, "psapi.lib")
 #if _WIN64
 #ifdef NDEBUG
@@ -18,9 +18,11 @@
 
 typedef int(__stdcall* MessageBoxW_t)(HWND, LPCWSTR, LPCWSTR, UINT);
 MessageBoxW_t originalMessageBoxW = nullptr;
-int __stdcall HookMessageBoxW(HWND hWnd, LPCWSTR lpCaption, LPCWSTR, UINT uType) {
+MessageBoxW_t pMessageBoxW;
+
+int __stdcall HookMessageBoxW(HWND hWnd, LPCWSTR lpCaption, LPCWSTR lpText, UINT uType) {
 	std::cout << "It Worked\n";
-	return originalMessageBoxW(hWnd, L"Hooked", L"info", uType);
+	return originalMessageBoxW(hWnd,lpCaption, lpText, uType);
 }
 
 void StartHook() {
@@ -29,11 +31,11 @@ void StartHook() {
 		std::cout << "Failed to initialize minhook\n";
 		return;
 	}
-	if (MH_CreateHook(&MessageBoxW, &HookMessageBoxW, reinterpret_cast<LPVOID*>(&originalMessageBoxW))!=MH_OK) {
-		std::cout << "Failed\n";
+	if (MH_CreateHook(pMessageBoxW, &HookMessageBoxW, reinterpret_cast<LPVOID*>(&originalMessageBoxW))!=MH_OK) {
+		std::cout << "Hook creation failed\n";
 		return;
 	}
-	if (MH_EnableHook(&MessageBoxW) != MH_OK) {
+	if (MH_EnableHook(pMessageBoxW) != MH_OK) {
 		std::cout << "Enable fail\n";
 		return;
 	}
@@ -41,8 +43,21 @@ void StartHook() {
 
 int main()
 {
+	HMODULE hUser32 = LoadLibrary(L"user32.dll");
+	if (!hUser32) {
+		std::cout << "Couldnt dynamically load user32.dll\n";
+		return 1;
+	}
+	pMessageBoxW = (MessageBoxW_t)GetProcAddress(hUser32, "MessageBoxW");
+	if (!pMessageBoxW) {
+		FreeLibrary(hUser32);
+		return 1;
+	}
+
 	StartHook();
-	MessageBoxW(NULL, L"Test", L"Test", MB_OK);
+	pMessageBoxW(0, L"Hello world!", L"Test", MB_OK);
+
 	MH_DisableHook(&MessageBoxW);
+	FreeLibrary(hUser32);
 	return 0;
 }
